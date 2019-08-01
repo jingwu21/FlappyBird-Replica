77e2386d8a88d62542c3dedf45fda96c6361505a
jingwu21 <jingwu721@gmail.com>
2019-08-01 01:58:39 -0500

    creating pipes

--- character.js
+++ character.js
@@ -4,6 +4,10 @@ class Bird{
       this.x = 50;
       this.radius = 15;
       this.color = "red";
+      this.gravity = 0.1;
+      this.velocity = 0;
+      this.height = height;
+      this.alive = true;
   }
   getCoordinates(){
      let coordinates = [this.x, this.y, this.radius];
@@ -12,8 +16,24 @@ class Bird{
   getColor(){
       return this.color;
   }
-  
+  fly(){
+    this.y -= 60;
+    this.velocity = 1;
+    console.log(this.y);
+  }
+
+  getLife(){
+     return this.alive;
+  }
+
   update(){
-     this.y -= 10;
+     
+     this.velocity += this.gravity;
+     this.y += this.velocity;
+     
+     if(this.y > this.height){
+        this.y = this.height;
+        this.alive = false;
+     }
   }
 }
--- map.js
+++ map.js
@@ -1,41 +1,61 @@
 var canvas = document.getElementById("myCanvas");
 var map = canvas.getContext("2d");
-const player = new Bird(500);
+const width = document.getElementById("myCanvas").width;
+const height = document.getElementById("myCanvas").height;
+const player = new Bird(height);
+var obstacles = [];
+console.log(width);
+
+
 function createMap(){
-  
+   
+   obstacles.push(new Pipe(height, width));  
    var playCoord =  player.getCoordinates();
    map.beginPath();
    map.arc(playCoord[0], playCoord[1], playCoord[2], 0, 2 * Math.PI);
    map.fillStyle = player.getColor();
    map.fill();
+   
 }
 
 function startGame(){
     document.onkeydown = control;
-    window.requestAnimationFrame(draw);
+    
+    
+    requestAnimationFrame(draw);
     
    
 }
 
 function draw(){
-  
+  if(!player.getLife())
+     return;
+  obstacles.push(new Pipe(height, width));  
+  var pipePos = obstacles[0].position();
+  var pipeWidth = pipePos[3];
+  player.update();
   var playCoord = player.getCoordinates();
    map.clearRect(0, 0, canvas.width, canvas.height)
    map.beginPath();
    map.arc(playCoord[0], playCoord[1], playCoord[2], 0, 2 * Math.PI);
    map.fillStyle = player.getColor();
    map.fill();
-   console.log(playCoord);
-   window.requestAnimationFrame(draw);
+   map.rect(0, 0 , pipeWidth, pipePos[1]);
+   map.stroke();
+  console.log(playCoord);
+  
+ 
+  requestAnimationFrame(draw);
+   
    
 }
 
 function control(event){
       var c = event || window.event;
       if(c.keyCode == '38'){
-         player.update();
+         player.fly();
          console.log(c.keyCode);
-         console.log(this.y);
+         
       }
 }
 //createMap();
